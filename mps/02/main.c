ff5242de3e7741e3e6767c94d278462136f869b5
Michael Lee <lee@soi2.org>
2015-05-30 13:55:41 -0500

    Adding hard puzzles

--- mps/02/main.c
+++ mps/02/main.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include "sudoku.h"
 
 int main(int argc, char *argv[]) {
-  int n=0, row;
+  int npuz=0, nval, c;
+  int has_headers = 0;
   char buf[80];
   unsigned char vals[9][9];
+
+  if ((c = fgetc(stdin)) == 'G') {
+    has_headers = 1;
+  } else {
+    ungetc(c, stdin);
+  }
+
   while (!feof(stdin)) {
-    if (!fgets(buf, sizeof(buf), stdin)) { // throw away puzzle heading
-      break; // in case EOF after previous newline
+    if (has_headers) {
+      fgets(buf, sizeof(buf), stdin);
     }
-    for (row=0; row<9; row++) {
-      fread(vals[row], 1, 9, stdin);
-      fgets(buf, sizeof(buf), stdin); // throw away rest of line
-    }
-    printf("Solving puzzle %d\n", ++n);
+
+    nval = 0;
+    do {
+      c = fgetc(stdin);
+      if (isdigit(c) || c == '.') {
+        *((char *)vals + nval++) = c;
+      } else if (c == EOF) {
+        goto done;
+      }
+    } while (nval<(9*9));
+
+    fgets(buf, sizeof(buf), stdin);
+
+    printf("Solving puzzle %d\n", ++npuz);
     solve(vals);
   }
+  
+ done:
   return 0;
 }
