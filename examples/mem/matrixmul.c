6a1460032eb486d78d196e63dde5b4c59a4d9c02
Michael Lee <lee@soi2.org>
2016-03-21 12:00:50 -0500

    Tweaking run-time args

--- examples/mem/matrixmul.c
+++ examples/mem/matrixmul.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#define DIM 500
-#define KIJ
+#define ITERS 10
+#define DIM   128
 
 int main(int argc, char *argv[]) {
   volatile int A[DIM][DIM], B[DIM][DIM], C[DIM][DIM], r;
-  int i, j, k, n, iters;
+  int i, j, k, n, imp;
 
   if (argc < 2) {
-    printf("Specify num iterations as argument\n");
+    printf("Specify algorithm {1=IJK, 2=KJI, 3=KIJ}\n");
     exit(1);
   } else {
-    iters = atoi(argv[1]);
+    imp = atoi(argv[1]);
   }
   
   for (i=0; i<DIM*DIM; i++) {
@@ -20,33 +21,36 @@ int main(int argc, char *argv[]) {
     ((int *)B)[i] = (int)random();
   }
 
-  for (n=0; n<iters; n++) {
-#ifdef IJK
-    for (i=0; i<DIM; i++) {
-      for (j=0; j<DIM; j++) {
-        C[i][j] = 0;
-        for (k=0; k<DIM; k++) {
-          C[i][j] += A[i][k] * B[k][j];
+  for (n=0; n<ITERS; n++) {
+    memset((void *)C, 0, DIM*DIM*sizeof(int));
+    switch(imp) {
+    case 1: /* IJK */
+      for (i=0; i<DIM; i++) {
+        for (j=0; j<DIM; j++) {
+          for (k=0; k<DIM; k++) {
+            C[i][j] += A[i][k] * B[k][j];
+          }
         }
       }
-    }
-#elif defined KJI
-    for (k=0; k<DIM; k++) {
-      for (j=0; j<DIM; j++) {
-        r = B[k][j];
-        for (i=0; i<DIM; i++)
-          C[i][j] += A[i][k]*r;
+      break;
+    case 2: /* KJI */
+      for (k=0; k<DIM; k++) {
+        for (j=0; j<DIM; j++) {
+          r = B[k][j];
+          for (i=0; i<DIM; i++)
+            C[i][j] += A[i][k]*r;
+        }
       }
-    }
-#elif defined KIJ
-    for (k=0; k<DIM; k++) {
-      for (i=0; i<DIM; i++) {
-        r = A[i][k];
-        for (j=0; j<DIM; j++)
-          C[i][j] += r*B[k][j];
+      break;
+    case 3: /* KIJ */
+      for (k=0; k<DIM; k++) {
+        for (i=0; i<DIM; i++) {
+          r = A[i][k];
+          for (j=0; j<DIM; j++)
+            C[i][j] += r*B[k][j];
+        }
       }
-    }
-#endif
+    } /* end switch */
   }
 
   return 0;
