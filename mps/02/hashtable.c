f5f93f48d07e067988fd37ba56cbbf357d98a834
Mayank Bansal <sunny.bansal@gmail.com>
2017-02-05 16:25:11 -0600

    Added functions

--- mps/02/hashtable.c
+++ mps/02/hashtable.c
@@ -5,65 +5,112 @@
 /* Daniel J. Bernstein's "times 33" string hash function, from comp.lang.C;
    See https://groups.google.com/forum/#!topic/comp.lang.c/lSKWXiuNOAk */
 unsigned long hash(char *str) {
-  unsigned long hash = 5381;
-  int c;
+    unsigned long hash = 5381;
+    int c;
 
-  while ((c = *str++))
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-  return hash;
+    return hash;
 }
 
 hashtable_t *make_hashtable(unsigned long size) {
-  hashtable_t *ht = malloc(sizeof(hashtable_t));
-  ht->size = size;
-  ht->buckets = calloc(sizeof(bucket_t *), size);
-  return ht;
+    hashtable_t *ht = malloc(sizeof(hashtable_t));
+    ht->size = size;
+    ht->buckets = calloc(sizeof(bucket_t *), size);
+    return ht;
 }
 
 void ht_put(hashtable_t *ht, char *key, void *val) {
-  /* FIXME: the current implementation doesn't update existing entries */
-  unsigned int idx = hash(key) % ht->size;
-  bucket_t *b = malloc(sizeof(bucket_t));
-  b->key = key;
-  b->val = val;
-  b->next = ht->buckets[idx];
-  ht->buckets[idx] = b;
+    /* FIXME: the current implementation doesn't update existing entries */
+    unsigned int idx = hash(key) % ht->size;
+    bucket_t *b = malloc(sizeof(bucket_t));
+
+
+    // Iterate though list and check for same key. If key exists, update value
+    int flag = 1;
+    for (bucket_t *itr = ht->buckets[idx]; itr->next != NULL; itr = itr->next) {
+        if (itr->key == key) {
+            itr->val = val;
+            flag--;
+            break;
+        }
+    }
+
+    // If flag is true, the key doesn't exist, so make new bucket
+    if (flag) {
+        b->key = key;
+        b->val = val;
+        b->next = ht->buckets[idx];
+        ht->buckets[idx] = b;
+    }
 }
 
 void *ht_get(hashtable_t *ht, char *key) {
-  unsigned int idx = hash(key) % ht->size;
-  bucket_t *b = ht->buckets[idx];
-  while (b) {
-    if (strcmp(b->key, key) == 0) {
-      return b->val;
+    unsigned int idx = hash(key) % ht->size;
+    bucket_t *b = ht->buckets[idx];
+    while (b) {
+        if (strcmp(b->key, key) == 0) {
+            return b->val;
+        }
+        b = b->next;
     }
-    b = b->next;
-  }
-  return NULL;
+    return NULL;
 }
 
 void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
-  bucket_t *b;
-  unsigned long i;
-  for (i=0; i<ht->size; i++) {
-    b = ht->buckets[i];
-    while (b) {
-      if (!f(b->key, b->val)) {
-        return ; // abort iteration
-      }
-      b = b->next;
+    bucket_t *b;
+    unsigned long i;
+    for (i = 0; i < ht->size; i++) {
+        b = ht->buckets[i];
+        while (b) {
+            if (!f(b->key, b->val)) {
+                return; // abort iteration
+            }
+            b = b->next;
+        }
     }
-  }
 }
 
 void free_hashtable(hashtable_t *ht) {
-  free(ht); // FIXME: must free all substructures!
+
+    for (int i = 0; i < ht->size; i++) {
+        bucket_t *curr;
+        while ((curr = ht->buckets[i]) != NULL) {
+            ht->buckets[i] = ht->buckets[i]->next;
+            free(curr->key);
+            free(curr->val);
+        }
+
+        free(curr);
+
+        free(ht->buckets[i]);
+    }
+
+    free(ht); // FIXME: must free all substructures!
 }
 
 /* TODO */
-void  ht_del(hashtable_t *ht, char *key) {
+void ht_del(hashtable_t *ht, char *key) {
+
+    unsigned int idx = hash(key) % ht->size;
+
+    // Iterate through list
+    for (bucket_t *prev = NULL, *itr = ht->buckets[idx]; itr->next != NULL; prev = itr, itr = itr->next) {
+        if (itr->key == key) {
+            // If clause for first element
+            if (prev == NULL) {
+                ht->buckets[idx] = itr->next;
+                free(itr);
+            } else {
+                prev->next = itr->next; //Point previous element to next element
+                free(itr);
+            }
+            break;
+        }
+    }
+
 }
 
-void  ht_rehash(hashtable_t *ht, unsigned long newsize) {
+void ht_rehash(hashtable_t *ht, unsigned long newsize) {
 }
