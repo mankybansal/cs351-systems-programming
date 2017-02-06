7a1a0555bd886ec3a23eefb3868d6b5e34532623
Mayank Bansal <sunny.bansal@gmail.com>
2017-02-05 20:10:47 -0600

    Mem Leak

--- mps/02/hashtable.c
+++ mps/02/hashtable.c
@@ -25,7 +25,7 @@ hashtable_t *make_hashtable(unsigned long size) {
 
 void ht_put(hashtable_t *ht, char *key, void *val) {
 
-     //FIXME: the current implementation doesn't update existing entries
+    //FIXME: the current implementation doesn't update existing entries
     unsigned int idx = hash(key) % ht->size;
     bucket_t *b = malloc(sizeof(bucket_t));
 
@@ -71,33 +71,17 @@ void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
     }
 }
 
-/*
-void print_ht(hashtable_t *ht) {
-    printf("\n------HASHTABLE--------\n");
+void free_hashtable(hashtable_t *ht) {
     bucket_t *b;
     unsigned long i;
     for (i = 0; i < ht->size; i++) {
         b = ht->buckets[i];
         while (b) {
-            printf("  %s->%s  ", b->key, b->val);
-            b = b->next;
-        }
-        printf("\n");
-    }
-
-    printf("\n----------------------\n");
-}*/
-
-void free_hashtable(hashtable_t *ht) {
-    bucket_t *b;
-    unsigned long i;
-    for (i=0; i<ht->size; i++) {
-        b = ht->buckets[i];
-        while(b){
-            ht_del(ht,b->key);
+            ht_del(ht, b->key);
             b = b->next;
         }
     }
+    free(b);
     free(ht->buckets);
 }
 
@@ -110,49 +94,20 @@ void ht_del(hashtable_t *ht, char *key) {
     for (; itr != NULL; prev = itr, itr = itr->next)
         if (strcmp(itr->key, key) == 0) {
             // If clause for first element
-            if (prev == NULL) {
+            if (prev == NULL)
                 ht->buckets[idx] = itr->next;
-                free(itr->key);
-                free(itr->val);
-                free(itr);
-            } else {
-                prev->next = itr->next; //Point previous element to next element
-                free(itr->key);
-                free(itr->val);
-                free(itr);
-            }
+            else
+                prev->next = itr->next;
+
+            free(itr->key);
+            free(itr->val);
+            free(itr);
             break;
         }
 
     if (ht->buckets[idx] == NULL)
         free(ht->buckets[idx]);
 }
-
-/*
-void print_ht_stats2(hashtable_t *ht) {
-    bucket_t *b;
-    unsigned long idx, len, max_len=0, num_buckets=0, num_chains=0;
-    for (idx=0; idx<ht->size; idx++) {
-        b = ht->buckets[idx];
-        len = 0;
-        while (b) {
-            len++;
-            num_buckets++;
-            b = b->next;
-        }
-        if (len > 0) {
-            num_chains++;
-        }
-        if (max_len < len) {
-            max_len = len;
-        }
-    }
-    printf("Num buckets = %lu\n", num_buckets);
-    printf("Max chain length = %lu\n", max_len);
-    printf("Avg chain length = %0.2f\n", (float)num_buckets / num_chains);
-    printf("\n\n\n\n");
-}
-*/
 
 void ht_rehash(hashtable_t *ht, unsigned long newsize) {
 
